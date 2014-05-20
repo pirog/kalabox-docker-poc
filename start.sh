b91e66abec229dd9f694dafa769e8cf23f3c2b3e
Mike Pirog <mike@kalamuna.com>
2014-05-19 17:55:45 -0700

    #11: Make sure DB is accesible from host

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ if [ ! $DBEXISTS -eq 0 ];then
   # This is so the passwords show up in logs.
   echo mysql root password: $MYSQL_PASSWORD
   mysqladmin -u $MYSQL_USER password $MYSQL_PASSWORD
-  mysql -u$MYSQL_PASSWORD -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DB; GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+  mysql -u$MYSQL_PASSWORD -p$MYSQL_PASSWORD -e "CREATE DATABASE $MYSQL_DB; GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
   sleep 10s
 fi
