8a44ebbd0bbdba7c9d00ca1f9a4b8450703e44dc
Mike Pirog <mike@kalamuna.com>
2014-05-22 22:50:59 -0700

    #16: Basic start script for OSX

---
+++ macosx/boot2docker.profile
@@ -0,0 +1,31 @@
+# Comments must be on their own lines; inline comments are not supported.
+
+# name of boot2docker virtual machine
+VM = "Kalabox"
+
+# path to boot2docker ISO image
+ISO = "$HOME/.boot2docker/boot2docker.iso"
+
+# VM disk image size in MB
+DiskSize = 20000
+
+# VM memory size in MB
+Memory = 2048
+
+# host-only network host IP
+HostIP = "1.3.3.7"
+
+# host only network network mask
+NetMask = [255, 255, 255, 0]
+
+# host-only network DHCP server IP
+DHCPIP = "1.3.3.1"
+
+# host-only network DHCP server enabled
+DHCPEnabled = true
+
+# host-only network IP range lower bound
+LowerIP = "1.3.3.8"
+
+# host-only network IP range upper bound
+UpperIP = "1.3.3.9"
---
+++ macosx/setup.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+#
+# Kalabox Server Install Script.
+#
+# Copyright (C) 2014 Kalamuna LLC
+#
+# This file will install the underlying dependencies required to run
+# kalastack-docker.
+#
+
+# Override any funny stuff from the user.
+export PATH="/bin:/usr/bin:/sbin:/usr/sbin:$PATH"
+
+## @param [Integer] $1 exit code.
+function key_exit() {
+    echo "Press enter to exit."
+    read
+    exit $1
+}
+
+##
+# Display some important knowledge first
+#
+echo ""
+echo "Welcome to the kalastack-docker server install script."
+echo ""
+echo "This script will help you get started running kalastack-docker"
+echo ""
+echo ""
+echo "WHATCHA WHATCHA WHATCHA WANT?"
+echo ""
+echo "1. Install everything I need and set up a webserver for me."
+echo "2. Install just the server dependencies."
+echo "3. Forget this ever happened."
+echo ""
+read my_answer
+if [ "$my_answer" == "3" ]; then
+    echo "This never happened."
+    key_exit 2
+fi
+
+# Initiate the actual uninstall, which requires admin privileges.
+echo "The installation process requires administrative privileges"
+echo "because some of the installed files cannot be removed by a"
+echo "normal user. You may now be prompted for a password..."
+
+# Just start the sudo party
+/usr/bin/sudo -p "Please enter %u's password:" echo "Let's get it started"
+
+# Install VirtualBox
+VBOX=$(which VBoxManage)
+if [ -z "$VBOX" ]; then
+    echo "Downloading VirtualBox..."
+    cd /tmp
+    /usr/bin/curl -O http://files.kalamuna.com/virtualbox-macosx-4.3.6.dmg
+    echo "Mounting VirtualBox..."
+    if [ -d /Volumes/VirtualBox ]; then
+        hdiutil detach /Volumes/VirtualBox -force
+    fi
+    hdiutil attach /tmp/virtualbox-macosx-4.3.6.dmg
+    echo "Installing VirtualBox..."
+    sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /Volumes/Macintosh\ HD
+    hdiutil detach /Volumes/VirtualBox -force
+    echo "VirtualBox Installed!"
+fi
+
+# Install Boot2Docker
+B2D=$(which boot2docker)
+if [ -z "$B2D" ]; then
+    echo "Downloading Boot2Docker..."
+    cd /tmp
+    /usr/bin/curl -O http://files.kalamuna.com/boot2docker-v0.9.2-darwin-amd64
+    echo "Installing Boot2Docker..."
+    sudo mv /tmp/boot2docker-v0.9.2-darwin-amd64 /usr/local/bin/boot2docker
+    sudo chmod +x /usr/local/bin/boot2docker
+    echo "Boot2Docker Installed!"
+fi
+
+#!/usr/bin/env bash
+if [ "$my_answer" == "1" ]; then
+    echo "Spinning up the hypervisor..."
+    if [ -a $HOME/.boot2docker/boot2docker.iso ]; then
+        rm $HOME/.boot2docker/boot2docker.iso
+    fi
+    cp -f boot2docker.profile /$HOME/.boot2docker/profile
+    /usr/local/bin/boot2docker init
+    /usr/local/bin/boot2docker up
+
+    # Get all our images
+    # Data only container
+    /usr/bin/curl -XPOST http://localhost:4243/images/create --data fromImage=busybox --data tag=latest
+    /usr/bin/curl -XPOST http://localhost:4243/images/create --data fromImage=pirog/kaladata-docker --data tag=latest
+    # Ubuntu 12.04
+    /usr/bin/curl -XPOST http://localhost:4243/images/create --data fromImage=ubuntu --data tag=12.04
+    # Reverse Proxy
+    /usr/bin/curl -XPOST http://localhost:4243/images/create --data fromImage=pirog/kalabox-proxy --data tag=latest
+    # Kalastack Docker
+    /usr/bin/curl -XPOST http://localhost:4243/images/create --data fromImage=pirog/kalastack-docker --data tag=12.04
+
+    # Set up the reverse proxy
+    #json=$(/usr/bin/curl -XPOST -H "Content-Type: application/json" http://localhost:4243/containers/create -d '
+    # {
+    # "Hostname":"proxy.kala",
+    # "User":"",
+    # "Memory":0,
+    # "MemorySwap":0,
+    # "AttachStdin":false,
+    # "AttachStdout":true,
+    # "AttachStderr":true,
+    # "PortSpecs":null,
+    # "Privileged": false,
+    # "Tty":false,
+    # "OpenStdin":false,
+    # "StdinOnce":false,
+    # "Env":null,
+    # "Dns":null,
+    # "Image":"pirog/kalabox-proxy",
+    # "Volumes":{},
+    # "VolumesFrom":"",
+    # "WorkingDir":""
+    #}')
+    #cid=$(echo $json | python -c 'import sys, json; print json.load(sys.stdin)[sys.argv[1]]' Id)
+    #/usr/bin/curl -XPOST http://localhost:4243/containers/$cid/start
+    #unset cid
+    #unset json
+fi
+
+echo "Done."
+key_exit 0;
+
